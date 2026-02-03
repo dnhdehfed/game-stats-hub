b8102a76209a72e6b783a4fe79dd7ec184aac188
dnhdehfed <xavier.pittman@education.nsw.gov.au>
2026-02-03 18:38:06 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/game-stats-hub/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
